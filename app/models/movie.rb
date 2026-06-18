be02b55326b44fc5e7983583521e589db5c99981
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-06-18 13:30:13 +0100

    update model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks,  dependent: :destroy
+  has_many :bookmarks
   has_many :lists, through: :bookmarks
+
+  validates :title, uniqueness: true
+  validates :overview, uniqueness: true
 end
