b7f01d82303877f923cb2e4a96fec6c540623190
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-06-18 13:34:43 +0100

    add validations to model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :lists, through: :bookmarks
 
+  validates :title, presence: true
+  validates :overview, presence: true
   validates :title, uniqueness: true
   validates :overview, uniqueness: true
 end
