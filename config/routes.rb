209c2028ef7b17cd43cea404a863b1b6b35fab29
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-06-24 23:44:30 +0100

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "lists#index"
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
